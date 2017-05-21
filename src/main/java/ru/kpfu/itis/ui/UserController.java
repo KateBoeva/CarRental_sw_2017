f918b78e6165e4e671ffa0fc733584addfff7616
katemrrr <katrinb_08@mail.ru>
2017-05-21 20:35:29 +0300

    update user service

--- src/main/java/ru/kpfu/itis/ui/UserController.java
+++ src/main/java/ru/kpfu/itis/ui/UserController.java
@@ -11,11 +11,18 @@ import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import ru.kpfu.itis.ConfigControllers;
 import ru.kpfu.itis.entity.Car;
+import ru.kpfu.itis.entity.Order;
 import ru.kpfu.itis.service.CarService;
+import ru.kpfu.itis.service.OrderService;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.util.List;
+
+import static ru.kpfu.itis.utils.Utils.createFrame;
 
 /**
  * Created by katemrrr on 12.05.17.
@@ -37,10 +44,21 @@ public class UserController {
 
     @FXML private TableView<Car> table;
 
-    private ObservableList<Car> data;
+    private ObservableList<Car> dataCar;
+    private ObservableList<Order> dataOrder;
 
     @Autowired
     private CarService carService;
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    FormOrderController formOrderController;
+
+    @Qualifier("formOrderView")
+    @Autowired
+    private ConfigControllers.View formOrderView;
 
     private Stage userStage;
 
@@ -56,8 +74,9 @@ public class UserController {
     @SuppressWarnings("unchecked")
     @PostConstruct
     public void init() {
+
         List<Car> cars = carService.findAll();
-        data = FXCollections.observableArrayList(cars);
+        dataCar = FXCollections.observableArrayList(cars);
 
         nameColumn = new TableColumn<>();
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("model"));
@@ -65,13 +84,38 @@ public class UserController {
         priceColumn = new TableColumn<>();
         priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
 
-//        table.getColumns().setAll(nameColumn, priceColumn);
-//        table.setItems(data);
+        table.getColumns().setAll(nameColumn, priceColumn);
+        table.setItems(dataCar);
+
+        table.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> showCarDetails(newValue)
+        );
+    }
+
+    private void showCarDetails(Car car){
+        car = table.getSelectionModel().getSelectedItem();
+        if (car != null){
+            modelLabel.setText(car.getName());
+            priceLabel.setText(String.valueOf(car.getPrice()));
+            runLabel.setText(String.valueOf(car.getRun()));
+            powerLabel.setText(String.valueOf(car.getPower()));
+            yearLabel.setText(String.valueOf(car.getYear()));
+            int k = 0;
+        }
     }
 
     @FXML
-    public void reserveCar(){
-
+    public void reserveCar() throws IOException{
+        Stage formOrderStage = new Stage();
+        createFrame("Добавить бронь", formOrderView, formOrderStage);
+        formOrderController.setFormOrderStage(formOrderStage);
+        formOrderController.setData(dataOrder);
+        formOrderController.clearOrder();
+        formOrderStage.show();
+        int k = 0;
     }
 
+    public ObservableList<Car> getDataCar() {
+        return dataCar;
+    }
 }
